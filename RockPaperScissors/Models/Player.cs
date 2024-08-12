b8f88117f12c9150009f5375e61fc9682b751462
igl-myrick <igl.myrick@gmail.com>
2024-08-12 14:08:08 -0700

    add CheckInput method

--- RockPaperScissors/Models/Player.cs
+++ RockPaperScissors/Models/Player.cs
@@ -4,6 +4,14 @@ namespace RockPaperScissors.Models
 {
   public class Player
   {
-    public string TurnInput { get; set; } = "rock";
+    public string TurnInput { get; set; }
+
+    public bool CheckInput()
+    {
+      if (TurnInput.ToLower() == "rock")
+      {
+        return true;
+      }
+    }
   }
 }
