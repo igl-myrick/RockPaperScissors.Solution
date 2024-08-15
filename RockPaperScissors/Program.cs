6533de98e6381835147c0f16102531266e70da79
igl-myrick <igl.myrick@gmail.com>
2024-08-15 13:37:46 -0700

    fix placement of methods

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -10,54 +10,54 @@ namespace RockPaperScissors
       Player player1 = new Player();
       Console.WriteLine("Rock Paper Scissors");
       HandleInput(player1);
-
-      static void HandleInput(Player player)
+    }
+    
+    static void HandleInput(Player player)
+    {
+      Console.WriteLine("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-");
+      Console.WriteLine("Enter 'Rock', 'Paper' or 'Scissors':");
+      player.TurnInput = Console.ReadLine().ToLower();
+      if (player.CheckInput() == false)
       {
-        Console.WriteLine("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-");
-        Console.WriteLine("Enter 'Rock', 'Paper' or 'Scissors':");
-        player.TurnInput = Console.ReadLine().ToLower();
-        if (player.CheckInput() == false)
+        Console.WriteLine("Incorrect input, please re-enter your choice:");
+        HandleInput(player);
+      }
+      else
+      {
+        string opponentResult = player.GetOpponentResult();
+        if (opponentResult == player.TurnInput)
         {
-          Console.WriteLine("Incorrect input, please re-enter your choice:");
-          HandleInput(player);
+          HandleTie(player, player.TurnInput, opponentResult);
+        }
+        else if (player.PlayerHasWon(opponentResult) == true)
+        {
+          Console.WriteLine("You win!");
+          HandleGameEnd(player.TurnInput, opponentResult);
         }
         else
         {
-          string opponentResult = player.GetOpponentResult();
-          if (opponentResult == player.TurnInput)
-          {
-            HandleTie(player, player.TurnInput, opponentResult);
-          }
-          else if (player.PlayerHasWon(opponentResult) == true)
-          {
-            Console.WriteLine("You win!");
-            HandleGameEnd(player.TurnInput, opponentResult);
-          }
-          else
-          {
-            Console.WriteLine("You lose!");
-            HandleGameEnd(player.TurnInput, opponentResult);
-          }
+          Console.WriteLine("You lose!");
+          HandleGameEnd(player.TurnInput, opponentResult);
         }
       }
+    }
 
-      static void HandleTie(Player player, string playerChoice, string opponentChoice)
-      {
-        Console.WriteLine("Tie!");
-        Console.WriteLine($"Your input: {playerChoice} | Opponent's choice: {opponentChoice}");
-        Console.WriteLine("Next round:");
-        HandleInput(player);
-      }
+    static void HandleTie(Player player, string playerChoice, string opponentChoice)
+    {
+      Console.WriteLine("Tie!");
+      Console.WriteLine($"Your input: {playerChoice} | Opponent's choice: {opponentChoice}");
+      Console.WriteLine("Next round:");
+      HandleInput(player);
+    }
 
-      static void HandleGameEnd(string playerChoice, string opponentChoice)
+    static void HandleGameEnd(string playerChoice, string opponentChoice)
+    {
+      Console.WriteLine($"Your input: {playerChoice} | Opponent's choice: {opponentChoice}");
+      Console.WriteLine("To play again, enter 'new'. To exit, enter any key.");
+      string userChoice = Console.ReadLine();
+      if (userChoice.ToLower() == "new")
       {
-        Console.WriteLine($"Your input: {playerChoice} | Opponent's choice: {opponentChoice}");
-        Console.WriteLine("To play again, enter 'new'. To exit, enter any key.");
-        string userChoice = Console.ReadLine();
-        if (userChoice.ToLower() == "new")
-        {
-          Main();
-        }
+        Main();
       }
     }
   }
